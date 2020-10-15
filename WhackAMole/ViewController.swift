09c7d3fa8574c7f063a5788e0494670d7ac74678
Jacob Wing <jacob.wing.680@k12.friscoisd.org>
2020-10-15 11:28:50 -0500

    Created a game over function

--- WhackAMole/ViewController.swift
+++ WhackAMole/ViewController.swift
@@ -4,11 +4,13 @@ class ViewController : UIViewController {
     var moleButton = UIButton()
     var scoreLabel = UILabel()
     var backgroundLabel = UILabel()
+    var gameOverLabel = UILabel()
     var timer = Timer()
     var screenWidth = 0
     var screenHeight = 0
     var score = 0
     var moles = 0
+    let interval = 5.0
     let margin = 20
     
     override func viewDidLoad() {
@@ -29,7 +31,7 @@ class ViewController : UIViewController {
         backgroundLabel.backgroundColor = UIColor.green
         
         //Button
-        moleButton.frame = CGRect(x: Int.random(in: margin...screenWidth-margin-40), y: Int.random(in: margin + (screenHeight/10)...screenHeight - margin), width:40, height:40)
+        moleButton.frame = CGRect(x: Int.random(in: margin...screenWidth-margin-40), y: Int.random(in: margin + (screenHeight/10)...screenHeight - margin - 40), width:40, height:40)
         moleButton.layer.cornerRadius = 20
         moleButton.backgroundColor = UIColor.brown
         moleButton.addTarget(self, action: #selector(hitMe(_:)), for: .touchUpInside)
@@ -43,14 +45,45 @@ class ViewController : UIViewController {
         self.view = view
     }
     @objc func hitMe(_ sender:UIButton!) {
-        print("Mole has been hit")
         score += 1
         moles += 1
         scoreLabel.text = "\(score)"
         moleButton.removeFromSuperview()
+        moleButton.frame = CGRect(x: Int.random(in: margin...screenWidth-margin-40), y: Int.random(in: margin + (screenHeight/10)...screenHeight - margin - 40), width:40, height:40)
+        view.addSubview(moleButton)
+        timer.invalidate()
+        if (moles >= 10) {
+            gameOver()
+        }
+        else {
+            timer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(missedMole), userInfo: nil, repeats: true)
+        }
     }
     @objc func missedMole() {
         moleButton.removeFromSuperview()
         moles += 1
+        moleButton.frame = CGRect(x: Int.random(in: margin...screenWidth-margin-40), y: Int.random(in: margin + (screenHeight/10)...screenHeight - margin - 40), width:40, height:40)
+        view.addSubview(moleButton)
+        if (moles >= 10) {
+            timer.invalidate()
+            gameOver()
+        }
+    }
+    @objc func gameOver() {
+        moleButton.removeFromSuperview()
+        backgroundLabel.removeFromSuperview()
+        
+        //Display game over text
+        gameOverLabel.frame = CGRect(x: margin, y: margin * 2, width: screenWidth - margin * 2, height: 30)
+        gameOverLabel.backgroundColor = UIColor.white
+        gameOverLabel.text = "Game Over"
+        gameOverLabel.textAlignment = NSTextAlignment.center
+        
+        scoreLabel.frame = CGRect(x: margin, y: margin * 4, width: screenWidth - margin * 2, height: 30)
+        scoreLabel.text = "Your scored \(score) points!"
+        scoreLabel.textAlignment = NSTextAlignment.center
+        
+        view.addSubview(gameOverLabel)
+        view.addSubview(scoreLabel)
     }
 }
